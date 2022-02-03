09c53c4a9b82f2c96c00af2aef09e57a11b86641
MeloDev <melodiaz2111@gmail.com>
2022-02-02 21:15:22 -0500

    change report view

--- public/js/IncidenciasController.js
+++ public/js/IncidenciasController.js
@@ -1,4 +1,4 @@
 function loadBI() {
   main = document.getElementById("main");
-  main.innerHTML = '<iframe title="Reportes_PowerBI - Página 1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e77efb72-2646-4201-b583-89e98817b306&autoAuth=true&ctid=957fe83a-7f3c-4d01-8235-50c8ee69e0ea&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWNlbnRyYWwtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="2" allowFullScreen="false"></iframe>'
+  main.innerHTML = '<iframe title="Reportes_PowerBI - Página 1" width="100%" height="541.25" src="https://app.powerbi.com/view?r=eyJrIjoiMjZkNzI3MzUtNTQ5MC00ODFiLTlkZTMtYzdhYjBlOGFkZjBiIiwidCI6Ijk1N2ZlODNhLTdmM2MtNGQwMS04MjM1LTUwYzhlZTY5ZTBlYSIsImMiOjR9&pageName=ReportSection" frameborder="2" allowFullScreen="true"></iframe>'
 }
