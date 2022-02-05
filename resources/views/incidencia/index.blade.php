168a1f173239df15583d40eff23f1564ec3c2cba
MeloDev <melodiaz2111@gmail.com>
2022-02-05 13:24:34 -0500

    view for show list of incidents

--- resources/views/incidencia/index.blade.php
+++ resources/views/incidencia/index.blade.php
@@ -4,8 +4,80 @@
 @endsection
 
 @section('contenido')
-  <div class="text-center bg-Light p-5 rounded">
+  <!--<div class="text-center bg-Light p-5 rounded">
     <h1 class="mt-4 pt-4">Listado de Incidencias</h1>
+  </div>
+  -->
+  <div class = "container p-2">
+    @if (Session::has('Mensaje'))
+        <div class="alert alert-success" role="alert">
+            {{ Session::get('Mensaje') }}
+        </div>
+    @endif
+    <div class = "row">
+        <div class ="col-md-8">
+            <h2>LISTADO DE INCIDENCIAS</h2>
+        </div>
+        <div class ="col-md-3">
+            <a class="btn btn-success btn-md" href="{{url('/incidencias/crear')}}">Crear Incidencia</a>
+        </div>
+    </div>
+    <div class="mt-3">
+        <table class = "table table-hover">
+            <thead class="table-primary">
+                <tr>
+                    <th>id</th>
+                    <th>Titulo</th>
+                    <th>Descripcion</th>
+                    <th>Usuario</th>
+                    <th>FechaCreacion</th>
+                    <th>Estado</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($incidencias as $item)
+                    <tr>
+                        <td>{{$item -> id}}</td>
+                        <td>{{$item -> titulo}}</td>
+                        <td>{{$item -> descripcion}}</td>
+                        <td>{{$item -> idUsuario}}</td>
+                        <td>{{$item -> created_at}}</td>
+                        <td>{{$item -> estado}}</td>
+
+                        <td>
+                        @switch($item -> estado)
+                          @case("Pendiente")
+                            <a class="btn btn-info btn-sm" href="{{url('/incidentes/'.$item->id.'/editar')}}">Editar</a>
+                            <form action="{{url('/incidentes/'.$item->id)}}" method="post">
+                                @csrf
+                                {{method_field('DELETE')}}
+
+                                <input type="submit" onclick="return confirm('Desea borrar?');" class="btn btn-danger btn-sm" value="Borrar"/>
+                            </form>
+                            <!-- Cuando se integre admin se borran estos botones-->
+                            <a class="btn btn-success btn-sm" href='/incidencias/aceptar/{{$item -> id}}'>Aceptar</a>
+                            <a class="btn btn-danger btn-sm" href='/incidencias/rechazar/{{$item -> id}}'>Rechazar</a>
+                            @break
+                          @case("PENDIENTE PARA ADMIN - cuando este roles hacemos esto")
+                            <!-- Cuando se integre admin se usaran estos botones-->
+                            <a class="btn btn-success btn-sm" href='/incidencias/aceptar/{{$item -> id}}'>Aceptar</a>
+                            <a class="btn btn-danger btn-sm" href='/incidencias/rechazar/{{$item -> id}}'>Rechazar</a>
+                            @break
+                          @case("Aceptado")
+                            <a class="btn btn-warning btn-sm" href='/incidencias/cerrar/{{$item -> id}}'>Cerrar</a>
+                            @break
+                          @default
+                        @endswitch
+                      </td>
+                    </tr>
+                @empty
+                    NO HAY INCIDENCIAS REGISTRADAS
+                @endforelse
+              </tbody>
+          </table>
+          {{$incidencias->links()}}
+      </div>
   </div>
 @endsection
 
